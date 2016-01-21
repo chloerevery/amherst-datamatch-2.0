0641e11b565f471252ecefdaf2ab7c1820330f48
chloerevery <chloerevery@gmail.com>
2016-01-20 21:09:37 -0500

    add validation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
+	$('#myForm').validator()
+
 	$('#button1').click(function(e) {
     	e.preventDefault();
     	$("#splash").toggleClass("hide");
      	$("#survey").toggleClass("hide");
     });
 
-    $('#button2').click(function(e) {
-    	e.preventDefault();
-     	$("#completed").toggleClass("hide");
-     	$("#survey").toggleClass("hide");
-    });
+    // $('#button2').click(function(e) {
+    // 	e.preventDefault();
+    //  	$("#completed").toggleClass("hide");
+    //  	$("#survey").toggleClass("hide");
+    // });
+
+	
+
+    $('#myForm').validator().on('submit', function (e) {
+	  if (e.isDefaultPrevented()) {
+	    $('body').animate({
+	    	scrollTop : 0
+	    },800);
+	  } else {
+	  	e.preventDefault();
+	    $("#completed").toggleClass("hide");
+    	$("#survey").toggleClass("hide");
+	  }
+	});
+
 });
