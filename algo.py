0733b2dfe57ab9c875d348b8c8a91afe18ff45a0
Evelyn Ting <eting17@amherst.edu>
2016-01-29 00:21:02 -0500

    updated algo

--- algo.py
+++ algo.py
@@ -4,7 +4,8 @@ db=client.amherst_datamatch
 participants=db.participants
 
 #MAY NEED TO CHANGE Question names
-questions = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+QUESTIONS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+MATRICES = ['straight', 'gay', 'lesbian', 'msnb', 'wsnb']
 straight_matrix = list()
 gay_matrix = list()
 lesbian_matrix = list()
@@ -14,16 +15,20 @@ women_seeking_nb = list()
 entry_dict = dict()
 entry_list = list()
 
-def makeMatrix (target_gender, seeking_gender):
+def makeMatrix (target_gender, seeking_gender, matrix_type):
         matrix = list()
         xdict = dict()
         xindex=0
         ydict = dict()
         yindex=0
         for x in participants.find( { 'gender' : target_gender , 'seeking': seeking_gender }):
+                _id = x['_id']
+                entry_dict[_id][matrix_type] = xindex
                 xdict[xindex] = x
                 xindex+=1
         for y in participants.find( { 'gender' : seeking_gender , 'seeking': target_gender }):
+                _id = y['_id']
+                entry_dict[_id][matrix_type] =yindex
                 ydict[yindex]=y
                 yindex+=1
 
@@ -33,7 +38,7 @@ def makeMatrix (target_gender, seeking_gender):
                 x_entry = xdict[x]
                 for y in range(yindex):
                         y_entry = ydict[y]
-                        for q in questions:
+                        for q in QUESTIONS:
                                 if x_entry[q]==y_entry[q]:
                                         matrix[x][y]+=1
         return matrix
@@ -58,22 +63,25 @@ def find_3best(target, num_seeking, matrix):
         return best_same
 
 def init_matrices():
+        global MATRICES
         for entry in participants.find():
-                name = entry['name']
-                entry_dict[name]=list()
+                name = entry['_id']
+                entry_dict[name]=dict()
                 entry_list.append(name)
         global straight_matrix
-        straight_matrix = makeMatrix('man', 'woman')
+        straight_matrix = makeMatrix('man', 'woman', MATRICES[0])
         global gay_matrix
-        gay_matrix = makeMatrix('man', 'man')
+        gay_matrix = makeMatrix('man', 'man', MATRICES[1])
         global lesbian_matrix
-        lesbian_matrix = makeMatrix('woman', 'woman')
+        lesbian_matrix = makeMatrix('woman', 'woman', MATRICES[2])
         global men_seeking_nb
-        men_seeking_nb = makeMatrix('man', 'nonbinary')
+        men_seeking_nb = makeMatrix('man', 'nonbinary', MATRICES[3])
         global women_seeking_nb
-        women_seeking_nb = makeMatrix('woman', 'nonbinary')
+        women_seeking_nb = makeMatrix('woman', 'nonbinary', MATRICES[4])
 
 #shuffle at end
 
 def main():
         init_matrices()
+
+main()
