5105058543e9143a112f8b8f9f9453ab1d584c42
Evelyn Ting <eting17@amherst.edu>
2016-01-30 20:43:33 -0500

    updated algo

--- algo.py
+++ algo.py
@@ -4,13 +4,9 @@ db=client.amherst_datamatch
 participants=db.participants
 
 #MAY NEED TO CHANGE Question names
-QUESTIONS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
-MATRICES = ['straight', 'gay', 'lesbian', 'msnb', 'wsnb']
-straight_matrix = list()
-gay_matrix = list()
-lesbian_matrix = list()
-men_seeking_nb = list()
-women_seeking_nb = list()
+QUESTIONS = ['class', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+MATRICES = [list(), list(), list(), list(), list()]      #an list of matrices
+XY_GUIDE = [['man', 'woman'], ['man', 'man'], ['woman', 'woman'], ['man', 'nonbinary'], ['woman', 'nonbinary']]
 
 entry_dict = dict()
 entry_list = list()
@@ -21,14 +17,16 @@ def makeMatrix (target_gender, seeking_gender, matrix_type):
         xindex=0
         ydict = dict()
         yindex=0
-        for x in participants.find( { 'gender' : target_gender , 'seeking': seeking_gender }):
+        for x in participants.find( { 'gender' : target_gender , seeking_gender: 1 }):
                 _id = x['_id']
                 entry_dict[_id][matrix_type] = xindex
+                entry_dict[_id]['gender']=target_gender
                 xdict[xindex] = x
                 xindex+=1
-        for y in participants.find( { 'gender' : seeking_gender , 'seeking': target_gender }):
+        for y in participants.find( { 'gender' : seeking_gender , target_gender: 1 }):
                 _id = y['_id']
                 entry_dict[_id][matrix_type] =yindex
+                entry_dict[_id]['gender']=seeking_gender
                 ydict[yindex]=y
                 yindex+=1
 
@@ -43,45 +41,56 @@ def makeMatrix (target_gender, seeking_gender, matrix_type):
                                         matrix[x][y]+=1
         return matrix
 
-def find_3best(target, num_seeking, matrix):
+
+def find3best(matrix, target, num_seeking):
         best_matches = [list(), list(), list()]
         best_same = [0, 0, 0]
         
         for seeking in range(num_seeking):
                 num_matches = matrix[target][seeking]
                 if num_matches>best_same[0]:
-                        best_same[2]=best_same[1]
-                        best_matches[2]=best_matches[1]
-                        best_same[1] = best_same[0]
-                        best_matches[1] =best_matches[0]
+                        for i in range(2, 0, -1):
+                                best_same[i]=best_same[i-1]
+                                best_matches[i]= list(best_matches[i-1])
                         best_same[0] = num_matches
-                        best_matches[0].append(num_matches)
                 for num in range (3):
                         if best_same[num]==num_matches:
-                                best_matches[num].append(num_matches)
+                                best_matches[num].append(seeking)
         best_same.append(best_matches)
         return best_same
 
 def init_matrices():
         global MATRICES
+        global XY_GUIDE
         for entry in participants.find():
                 name = entry['_id']
                 entry_dict[name]=dict()
                 entry_list.append(name)
-        global straight_matrix
-        straight_matrix = makeMatrix('man', 'woman', MATRICES[0])
-        global gay_matrix
-        gay_matrix = makeMatrix('man', 'man', MATRICES[1])
-        global lesbian_matrix
-        lesbian_matrix = makeMatrix('woman', 'woman', MATRICES[2])
-        global men_seeking_nb
-        men_seeking_nb = makeMatrix('man', 'nonbinary', MATRICES[3])
-        global women_seeking_nb
-        women_seeking_nb = makeMatrix('woman', 'nonbinary', MATRICES[4])
+        for i in range(5):
+                MATRICES[i]=makeMatrix(XY_GUIDE[i][0], XY_GUIDE[i][1], i)
+                print MATRICES[i]
 
 #shuffle at end
-
 def main():
+        global entry_list
+        global entry_dict
+        global MATRICES
+        global XY_GUIDE
         init_matrices()
+        for _id in entry_list:
+                matrixnums_to_coords = entry_dict[_id]
+                for matrixnum in range(5):
+                        if matrixnum in matrixnums_to_coords:
+                                matrix = MATRICES[matrixnum]
+                                target = matrixnums_to_coords[matrixnum]
+                                num_seeking=0
+                                gender_combo = XY_GUIDE[matrixnum]
+                                target_gender = matrixnums_to_coords['gender']
+                                if gender_combo[0]==target_gender:
+                                        num_seeking = len(matrix[0])
+                                else:
+                                        num_seeking = len(matrix)
+                                matches = find3best(matrix, target, num_seeking)
+                                print matches
 
 main()
--- inserting.py
+++ inserting.py
@@ -18,6 +18,17 @@ def starting_page():
 def insert_participant():
     _id= request.form['email']
     id_end = len(_id)-12
+    seeking_man=0
+    seeking_woman=0
+    seeking_nonbinary=0
+    seeking = request.form.getlist('seeking')
+    if "0" in seeking:
+        seeking_man=1
+    if "1" in seeking:
+        seeking_woman=1
+    if "2" in seeking:
+        seeking_nonbinary=1
+            
     participant = {
         "_id" : _id[0 : id_end],
         "name": request.form['name'],
@@ -25,7 +36,9 @@ def insert_participant():
         "phone":request.form['phone'],
         "class": request.form['class'],
         "gender":request.form['gender'],
-        "seeking":request.form['seeking'],
+        "man": seeking_man,
+        "woman": seeking_woman,
+        "nonbinary": seeking_nonbinary,
         "one": request.form["one"],
         "two": request.form["two"],
         "three": request.form["three"],
