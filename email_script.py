94825fabed30114af4707a0240a299f2fc4a1443
chloerevery <chloerevery@gmail.com>
2016-01-31 02:31:39 -0500

    updated script

--- email_script.py
+++ email_script.py
@@ -19,8 +19,6 @@ message.add_bcc('acdatamatch@gmail.com') #not working?
 message.set_subject(':name, Your True Love Matches!')
 #set html
 message.set_html('Hi :name,<br><br> Your matches are: <br><br> 1. :match1, :phone1 <br> 2. :match2, :phone2 <br> 3. :match3, :phone3. <br><br>You\'re super compatible! Shoot them a text and hang out sometime!')
-#set 'send to'
-message.smtpapi.add_to(['crevery16@amherst.edu', 'rcroll16@amherst.edu'])
 
 #get match names and emails for each entry in the database, one entry at a time
 
@@ -35,6 +33,8 @@ second_phones = [] #create empty array for second matches' phone numbers
 third_phones = [] #create empty array for third matches' phone numbers
     
 for entry in participants.find():
+    emails+= ??? #append the participant's email to the emails array
+    
     names+= ??? #append the entry's name to the names array
     
     first_matches+= ??? #append that person's first match to the first_match array
@@ -45,6 +45,9 @@ for entry in participants.find():
     second_phones+= ??? #append the second match's phone number to the second_phones array
     third_matches+= ??? #append the third match's phone number to the third_phones array
 
+#set 'send to'
+message.smtpapi.add_to(emails)
+
 #set the substitutions for the email template
 message.set_substitutions({':name': names,
                            ':match1': first_matches,
@@ -54,9 +57,6 @@ message.set_substitutions({':name': names,
                            ':phone2': second_phones,
                            ':phone3': third_phones
                            })
-
-
-
 
 #send that wildebeast!
 sg.send(message)
