8207167256c7cf97bc45f38c96ce15fadc35161e
chloerevery <chloerevery@gmail.com>
2016-02-02 17:53:19 -0500

    Update email_script.py

--- email_script.py
+++ email_script.py
@@ -10,7 +10,7 @@ participants=db.participants
 matches = db.matches
 
 #create email message
-sg = sendgrid.SendGridClient('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')
+sg = sendgrid.SendGridClient('')
 message = sendgrid.Mail()
 message.set_headers({'X-Sent-Using': 'SendGrid-API', 'X-Transport': 'web'});
 message.set_from('acdatamatch@gmail.com')
